deaee1ee599f6aae9b4ae762432ef2d8b533c87a
PaulVisscher <paul.pev@hotmail.com>
2016-08-18 13:01:55 +0200

    Fix workind directory

--- boost.lua
+++ boost.lua
@@ -1,9 +1,13 @@
 
+local oldPath = os.getcwd()
+os.chwd( zpm.build._currentDependency.dependencyPath )
+
 if os.is( "windows" ) then
-    print(path.join( zpm.build._currentDependency.dependencyPath, "bootstrap.bat" ) )
-    os.execute( path.join( zpm.build._currentDependency.dependencyPath, "bootstrap.bat" ) )
-    os.execute( path.join( zpm.build._currentDependency.dependencyPath, "b2.exe -j 8 headers" ) )
+    os.execute( "bootstrap.bat" )
+    os.execute( "b2.exe -j 8 headers" )
 else
-    os.execute( path.join( zpm.build._currentDependency.dependencyPath, "bootstrap.sh" ) )
-    os.execute( path.join( zpm.build._currentDependency.dependencyPath, "b2 -j 8 headers") )
+    os.execute( "bootstrap.sh" )
+    os.execute( "b2 -j 8 headers")
 end
+
+os.chwd( oldPath )
