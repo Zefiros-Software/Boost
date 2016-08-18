293f704febab31e32bfba47104937637ed2c7f5c
PaulVisscher <paul.pev@hotmail.com>
2016-08-18 12:58:14 +0200

    Debug

--- boost.lua
+++ boost.lua
@@ -1,7 +1,8 @@
 
 if os.is( "windows" ) then
+    print(path.join( zpm.build._currentDependency.dependencyPath, "bootstrap.bat" ) )
     os.execute( path.join( zpm.build._currentDependency.dependencyPath, "bootstrap.bat" ) )
-    os.execute( path.join( zpm.build._currentDependency.dependencyPath, "b2 -j 8 headers" ) )
+    os.execute( path.join( zpm.build._currentDependency.dependencyPath, "b2.exe -j 8 headers" ) )
 else
     os.execute( path.join( zpm.build._currentDependency.dependencyPath, "bootstrap.sh" ) )
     os.execute( path.join( zpm.build._currentDependency.dependencyPath, "b2 -j 8 headers") )
