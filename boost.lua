4ce26089b177f592c1ce1fdbd8a3b715a7b6644f
PaulVisscher <paul.pev@hotmail.com>
2016-08-18 19:24:41 +0200

    User execute permission

--- boost.lua
+++ boost.lua
@@ -7,7 +7,7 @@ if os.is( "windows" ) then
     os.execute( "b2.exe -j 8 headers" )
 else
     os.execute( "bash bootstrap.sh" )
-    os.execute( "chmod a+x b2" )
+    os.execute( "chmod u+x b2" )
     os.execute( "./b2 -j 8 headers")
 end
 
