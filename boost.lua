b61e43bc6d5a7e6411b7b272aa56a5db931c5b6c
PaulVisscher <paul.pev@hotmail.com>
2016-08-18 12:44:08 +0200

    Directory fix

--- boost.lua
+++ boost.lua
@@ -1,8 +1,8 @@
 
 if os.is( "windows" ) then
-    os.execute( path.join( os.getcwd(), "bootstrap.bat" ) )
-    os.execute( path.join( os.getcwd(), "b2 -j 8 headers" ) )
+    os.execute( path.join( zpm.build._currentExportPath, "bootstrap.bat" ) )
+    os.execute( path.join( zpm.build._currentExportPath, "b2 -j 8 headers" ) )
 else
-    os.execute( path.join( os.getcwd(), "bootstrap.sh" ) )
-    os.execute( path.join( os.getcwd(), "b2 -j 8 headers") )
+    os.execute( path.join( zpm.build._currentExportPath, "bootstrap.sh" ) )
+    os.execute( path.join( zpm.build._currentExportPath, "b2 -j 8 headers") )
 end
