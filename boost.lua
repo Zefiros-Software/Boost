a66fe6cd4e4366e1ce5e55e23433de17110aad0a
PaulVisscher <paul.pev@hotmail.com>
2016-08-18 12:06:49 +0200

    Update extraction

--- boost.lua
+++ boost.lua
@@ -1,8 +1,8 @@
 
 if os.is( "windows" ) then
     os.execute( "bootstrap" )
-    os.executef( "b2 --prefix=%s -j 8 headers", zpm.build.getExportDir() )
+    os.execute( "b2 -j 8 headers" )
 else
     os.execute( "./bootstrap.sh" )
-    os.executef( "./b2 --prefix=%s -j 8 headers", zpm.build.getExportDir() )
+    os.execute( "./b2 -j 8 headers")
 end
