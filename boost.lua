6b1cfe98566945cb0425604a406f86b89e941f40
PaulVisscher <paul.pev@hotmail.com>
2016-08-18 19:18:23 +0200

    Fix permissions

--- boost.lua
+++ boost.lua
@@ -6,9 +6,8 @@ if os.is( "windows" ) then
     os.execute( "bootstrap.bat" )
     os.execute( "b2.exe -j 8 headers" )
 else
-    print( os.getcwd(), os.isfile( "bootstrap.sh" ) )
     os.execute( "bash bootstrap.sh" )
-    os.execute( "ls" )
+    os.execute( "chmod a+x b2" )
     os.execute( "./b2 -j 8 headers")
 end
 
