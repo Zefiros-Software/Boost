7156487de93e1bf17a4e2957d524e9ea386b975a
PaulVisscher <paul.pev@hotmail.com>
2016-08-18 19:36:10 +0200

    Debug statement

--- boost.lua
+++ boost.lua
@@ -7,7 +7,7 @@ if os.is( "windows" ) then
     os.execute( "b2.exe -j 8 headers" )
 else
     os.execute( "bash bootstrap.sh" )
-    os.execute( "ll" )
+    os.execute( "ls -a" )
     os.execute( "chmod u+rx b2" )
     os.execute( "./b2 -j 8 headers")
 end
