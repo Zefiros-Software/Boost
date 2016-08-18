fa1552d55143ebbf1b8a772b7cffe83a25fe842a
PaulVisscher <paul.pev@hotmail.com>
2016-08-18 13:29:46 +0200

    Correct prefix

--- boost.lua
+++ boost.lua
@@ -4,10 +4,10 @@ os.chdir( zpm.build._currentDependency.dependencyPath )
 
 if os.is( "windows" ) then
     os.execute( "bootstrap.bat" )
-    os.execute( "b2.exe -j 8 headers" )
+    os.execute( "b2.exe --prefix=boost -j 8 headers" )
 else
     os.execute( "bootstrap.sh" )
-    os.execute( "b2 -j 8 headers")
+    os.execute( "b2 --prefix=boost -j 8 headers")
 end
 
 os.chdir( oldPath )
