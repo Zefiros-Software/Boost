ce01cf338dc763029ca1eaa97a8b0bdf308954f9
PaulVisscher <paul.pev@hotmail.com>
2016-09-01 20:12:42 +0200

    Speedup multiple boost uses

--- boost.lua
+++ boost.lua
@@ -3,10 +3,16 @@ local oldPath = os.getcwd()
 os.chdir( zpm.build._currentDependency.dependencyPath )
 
 if os.is( "windows" ) then
-    os.execute( "bootstrap.bat" )
+    if os.isfile( "b2.exe" ) == false then
+        os.execute( "bootstrap.bat" )
+    end
+    
     os.execute( "b2.exe -j 8 headers" )
 else
-    os.execute( "bash bootstrap.sh" )
+    if os.isfile( path.join( zpm.build._currentDependency.dependencyPath, "b2" ) ) == false then
+        os.execute( "bash bootstrap.sh" )
+    end
+    
     os.execute( path.join( zpm.build._currentDependency.dependencyPath, "b2 -j 8 headers" ) )
 end
 
